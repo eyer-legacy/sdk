4c032de161b4bf2c60e37dd8d7b49b3492548e41
Alexey Ostrovsky <d4ruko@gmail.com>
2016-07-04 11:30:43 +0200

    Added string values for LinkType enumeration members.

--- Aims.Sdk/LinkType.cs
+++ Aims.Sdk/LinkType.cs
@@ -1,10 +1,19 @@
-﻿namespace Aims.Sdk
+﻿using System.Runtime.Serialization;
+
+namespace Aims.Sdk
 {
     public enum LinkType
     {
+        [EnumMember(Value = "hierarchy")]
         Hierarchy = 1,
+
+        [EnumMember(Value = "binding")]
         Binding = 2,
+
+        [EnumMember(Value = "reference")]
         Reference = 3,
+
+        [EnumMember(Value = "dynamic")]
         Dynamic = 4,
     }
 }
